33b499d0d4593ff66f0fe98d16afe435181ea68b
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2023-07-25 14:49:47 +0530

    user created

---
+++ src/main/java/com/lead/dashboard/controller/UserController/UserController.java
@@ -0,0 +1,72 @@
+package com.lead.dashboard.controller.UserController;
+
+import com.lead.dashboard.domain.User;
+import com.lead.dashboard.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUserData()
+    {
+    List<User> allUser=userService.getAllUsers();
+    if(!allUser.isEmpty())
+    {
+        return  new ResponseEntity<>(allUser,HttpStatus.OK);
+    }
+    else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    }
+
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        User existingUser = userService.getUserById(id);
+        if (existingUser != null) {
+            user.setId(id);
+            User updatedUser = userService.updateUser(user);
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        User existingUser = userService.getUserById(id);
+        if (existingUser != null) {
+            userService.deleteUser(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
