72acba3bd60714eb4f1df2d7273ed56246d55b9c
corpseedBackend <ajay.kumar@corpseed.com>
2023-07-29 15:49:51 +0530

    add communication issue

--- src/main/java/com/lead/dashboard/repository/StatusRepository.java
+++ src/main/java/com/lead/dashboard/repository/StatusRepository.java
@@ -2,9 +2,13 @@ package com.lead.dashboard.repository;
 
 import com.lead.dashboard.domain.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StatusRepository extends JpaRepository<Status,Long>
 {
     Status getStatusById(Long id);
+
+	Status findAllByName(String status);
 
 }
--- src/main/java/com/lead/dashboard/service/LeadService.java
+++ src/main/java/com/lead/dashboard/service/LeadService.java
@@ -1,5 +1,6 @@
 package com.lead.dashboard.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -7,11 +8,15 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lead.dashboard.domain.Client;
 import com.lead.dashboard.domain.Lead;
+import com.lead.dashboard.domain.Status;
 import com.lead.dashboard.domain.User;
 import com.lead.dashboard.dto.LeadDto;
 import com.lead.dashboard.dto.UpdateLeadDto;
+import com.lead.dashboard.repository.ClientRepository;
 import com.lead.dashboard.repository.LeadRepository;
+import com.lead.dashboard.repository.StatusRepository;
 import com.lead.dashboard.repository.UserRepo;
 
 //import java.util.ArrayList;
@@ -30,6 +35,12 @@ public class LeadService {
 	LeadRepository leadRepository;
 	@Autowired
 	UserRepo UserRepo;
+	
+	@Autowired
+	StatusRepository statusRepository;
+	
+	@Autowired
+	ClientRepository clientRepository;
 	
 
 	public  Lead createEnquiryLead(String name,String mobNo,String desc) {
@@ -62,11 +73,13 @@ public class LeadService {
         l.setContacts(leadDto.getContacts());
         l.setCreateDate(new Date());
         l.setLastUpdated(leadDto.getLastUpdated());
+        Status status=statusRepository.findAllByName(leadDto.getStatus());
         if(opUser!=null && opUser.get()!=null) {
              l.setUpdatedBy(opUser.get());
              l.setCreatedBy(opUser.get());
 
         }
+        l.setStatus(status);
         l.setLatestStatusChangeDate(new Date());
         l.setPrimaryAddress(leadDto.getPrimaryAddress());
          l.setSource(leadDto.getSource());
@@ -77,8 +90,14 @@ public class LeadService {
 	public Lead updateLeadData(UpdateLeadDto updateLeadDto) {
 		Optional<Lead> lead = leadRepository.findById(updateLeadDto.getId());  
 		Lead updatedLead  = new Lead();
+		
 		if(lead!=null) {
 			Lead l = lead.get();
+			Optional<Status> opStatus = statusRepository.findById(updateLeadDto.getStatusId());
+			Status status = opStatus.get();
+			if(status!=null) {
+				l.setStatus(status);
+			}
 			l.setLeadName(updateLeadDto.getLeadName());
 			l.setLeadDescription(updateLeadDto.getLeadDescription());
 	        l.setContacts(updateLeadDto.getContacts());
@@ -104,6 +123,35 @@ public class LeadService {
 		}
 		return flag;
 	}
+	
+//	String name;
+//	String emails;
+//	String contactNo;
+	
+	public Lead createClientInLead(Long leadId,String name ,String email,String contactNo) {
+		Client client = new Client();
+		client.setName(name);
+		client.setContactNo(contactNo);
+		client.setEmails(email);
+//		clientRepository.save(client);
+		Optional<Lead> opLead = leadRepository.findById(leadId);
+		Lead lead = opLead.get();
+		if(lead.getClients()!=null) {
+			List<Client> clientList = lead.getClients();
+			clientList.add(client);
+			lead.setClients(clientList);
+			leadRepository.save(lead);
+		}else {
+			List<Client> clientList =new ArrayList<>();
+			clientList.add(client);
+			lead.setClients(clientList);
+			leadRepository.save(lead);
+		}
+		
+		return lead;
+		
+	}
+
 
 
 
