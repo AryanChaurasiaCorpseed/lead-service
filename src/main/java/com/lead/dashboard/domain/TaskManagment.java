2101496a4e6bdd6dbce50f15f49db9b871a13053
corpseedBackend <ajay.kumar@corpseed.com>
2023-08-11 16:32:31 +0530

    resolved issue

--- src/main/java/com/lead/dashboard/domain/TaskManagment.java
+++ src/main/java/com/lead/dashboard/domain/TaskManagment.java
@@ -1,18 +1,27 @@
 package com.lead.dashboard.domain;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 /*
 @Author :-Aryan Chaurasia
 
 */
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Table(name = "task_managment")
 public class TaskManagment {
 	
 	@Id
@@ -23,6 +32,9 @@ public class TaskManagment {
 	
 	@OneToOne
 	User assignedBy;
+	
+	@OneToOne
+	TaskStatus TaskStatus;
 	
 	@OneToOne
 	User assigne;
