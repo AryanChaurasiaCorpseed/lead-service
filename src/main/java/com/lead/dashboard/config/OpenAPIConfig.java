598c8155840e538c3d5855caba4c3015df6fc63b
corpseedBackend <ajay.kumar@corpseed.com>
2023-09-04 12:40:29 +0530

    add some changes in url

--- src/main/java/com/lead/dashboard/config/OpenAPIConfig.java
+++ src/main/java/com/lead/dashboard/config/OpenAPIConfig.java
@@ -15,9 +15,9 @@ import io.swagger.v3.oas.models.servers.Server;
 public class OpenAPIConfig {
 
 
-  private String devUrl="http://localhost:9090";;
+  private String devUrl="http://localhost:8089";;
 
-  private String prodUrl="http://localhost:9090";
+  private String prodUrl="http://localhost:8089";
 
   @Bean
   public OpenAPI myOpenAPI() {
