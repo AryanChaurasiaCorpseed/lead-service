6ec4ea45ca683a76b78471b52dcb2dc55f247d35
corpseedBackend <ajay.kumar@corpseed.com>
2023-09-26 11:58:42 +0530

    resolved issue

--- src/main/java/com/lead/dashboard/controller/inboxController/InboxController.java
+++ src/main/java/com/lead/dashboard/controller/inboxController/InboxController.java
@@ -5,12 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lead.dashboard.domain.Communication;
 import com.lead.dashboard.service.inboxService.InboxService;
 
 @RestController
+@RequestMapping("/leadService/")
 public class InboxController {
 	
 	@Autowired
--- src/main/java/com/lead/dashboard/controller/taskManagmentController/TaskManagmentController.java
+++ src/main/java/com/lead/dashboard/controller/taskManagmentController/TaskManagmentController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ import com.lead.dashboard.service.taskManagmentService.TaskManagmentService;
 
 */
 @RestController
+@RequestMapping("/leadService/")
 public class TaskManagmentController {
 	
 	@Autowired
