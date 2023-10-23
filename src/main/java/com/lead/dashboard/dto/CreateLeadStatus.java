5112a8cdc6a424c36ecf28596b662c8c8e21f983
corpseedBackend <ajay.kumar@corpseed.com>
2023-10-23 13:43:45 +0530

    changes in Lead status Api

---
+++ src/main/java/com/lead/dashboard/dto/CreateLeadStatus.java
@@ -0,0 +1,27 @@
+package com.lead.dashboard.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateLeadStatus {
+
+	 private String name;
+
+	    private String description;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+}
