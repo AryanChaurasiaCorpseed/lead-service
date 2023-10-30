1687c71b8da863cec7dbbf692d710958a98ee8f6
corpseedBackend <ajay.kumar@corpseed.com>
2023-10-30 12:10:52 +0530

    resolved issue

--- src/main/java/com/lead/dashboard/dto/AddProductInLead.java
+++ src/main/java/com/lead/dashboard/dto/AddProductInLead.java
@@ -8,19 +8,7 @@ public class AddProductInLead {
 	Long productId;
 	Long leadId;
 	String serviceName;
-	
-	int  govermentfees;
-    String govermentCode;
-    String govermentGst;
-    int professionalFees;
-    String professionalCode;
-    String profesionalGst;
-    int serviceCharge;
-    String serviceCode;
-    String serviceGst;
-    int otherFees;
-    String otherCode;
-    String otherGst;     
+  
     
 	public Long getProductId() {
 		return productId;
@@ -40,79 +28,6 @@ public class AddProductInLead {
 	public void setServiceName(String serviceName) {
 		this.serviceName = serviceName;
 	}
-	public int getGovermentfees() {
-		return govermentfees;
-	}
-	public void setGovermentfees(int govermentfees) {
-		this.govermentfees = govermentfees;
-	}
-	public String getGovermentCode() {
-		return govermentCode;
-	}
-	public void setGovermentCode(String govermentCode) {
-		this.govermentCode = govermentCode;
-	}
-	public String getGovermentGst() {
-		return govermentGst;
-	}
-	public void setGovermentGst(String govermentGst) {
-		this.govermentGst = govermentGst;
-	}
-	public int getProfessionalFees() {
-		return professionalFees;
-	}
-	public void setProfessionalFees(int professionalFees) {
-		this.professionalFees = professionalFees;
-	}
-	public String getProfessionalCode() {
-		return professionalCode;
-	}
-	public void setProfessionalCode(String professionalCode) {
-		this.professionalCode = professionalCode;
-	}
-	public String getProfesionalGst() {
-		return profesionalGst;
-	}
-	public void setProfesionalGst(String profesionalGst) {
-		this.profesionalGst = profesionalGst;
-	}
-	public int getServiceCharge() {
-		return serviceCharge;
-	}
-	public void setServiceCharge(int serviceCharge) {
-		this.serviceCharge = serviceCharge;
-	}
-	public String getServiceCode() {
-		return serviceCode;
-	}
-	public void setServiceCode(String serviceCode) {
-		this.serviceCode = serviceCode;
-	}
-	public String getServiceGst() {
-		return serviceGst;
-	}
-	public void setServiceGst(String serviceGst) {
-		this.serviceGst = serviceGst;
-	}
-	public int getOtherFees() {
-		return otherFees;
-	}
-	public void setOtherFees(int otherFees) {
-		this.otherFees = otherFees;
-	}
-	public String getOtherCode() {
-		return otherCode;
-	}
-	public void setOtherCode(String otherCode) {
-		this.otherCode = otherCode;
-	}
-	public String getOtherGst() {
-		return otherGst;
-	}
-	public void setOtherGst(String otherGst) {
-		this.otherGst = otherGst;
-	}
-	
 	
 //	
 	
