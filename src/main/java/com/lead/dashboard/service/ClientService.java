a5348451bb9a64b5b4a0eaf475e2dbf3dfbd1003
corpseedBackend <ajay.kumar@corpseed.com>
2023-10-31 13:35:58 +0530

    update Client Api in Some Modification

--- src/main/java/com/lead/dashboard/service/ClientService.java
+++ src/main/java/com/lead/dashboard/service/ClientService.java
@@ -1,13 +1,17 @@
 package com.lead.dashboard.service;
 
+import org.springframework.stereotype.Service;
+
 import com.lead.dashboard.domain.Client;
 import com.lead.dashboard.domain.lead.Lead;
+import com.lead.dashboard.dto.UpdateClientDto;
 
+@Service
 public interface ClientService {
 
 //    void removeClientFromLead(Long leadId, Long clientId);
 
-    Client updateClientInfo(Client client);
+    Client updateClientInfo(UpdateClientDto client);
 
     Lead createClientInLead(Long leadId, String name, String email, String contactNo);
 
