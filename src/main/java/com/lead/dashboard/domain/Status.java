827a51a76b3264b44f44c6b10fe3840c4c377892
corpseedBackend <ajay.kumar@corpseed.com>
2023-11-08 15:49:14 +0530

    add new Company features

--- src/main/java/com/lead/dashboard/domain/Status.java
+++ src/main/java/com/lead/dashboard/domain/Status.java
@@ -1,18 +1,16 @@
 package com.lead.dashboard.domain;
 
 
-import io.swagger.annotations.ApiModelProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
 @Data
 @Table(name = "status_details")
 public class Status {
