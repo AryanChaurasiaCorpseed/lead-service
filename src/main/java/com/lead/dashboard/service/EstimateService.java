26af55854d207ffe145b8457453f00eaa8e3da5d
corpseedBackend <ajay.kumar@corpseed.com>
2023-11-22 18:30:05 +0530

    add some changes

--- src/main/java/com/lead/dashboard/service/EstimateService.java
+++ src/main/java/com/lead/dashboard/service/EstimateService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import com.lead.dashboard.domain.ServiceDetails;
 import com.lead.dashboard.domain.lead.Lead;
 import com.lead.dashboard.dto.CreateServiceDetails;
+import com.lead.dashboard.dto.EditEstimate;
+import com.lead.dashboard.dto.EditEstimateAddress;
 
 @Service
 public interface EstimateService {
@@ -16,5 +18,9 @@ public interface EstimateService {
 	List<ServiceDetails> getAllEstimate();
 
 	ServiceDetails getEstimate(Long estimateId);
+
+	ServiceDetails editEstimateInvoice(EditEstimate editEstimate);
+
+	ServiceDetails editEstimateAddress(EditEstimateAddress editEstimateAddress);
 
 }
