21e7bd83174a9421a18ba2b0266156724a7568c4
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2023-12-06 10:55:23 +0530

    add Some changes

--- src/main/java/com/lead/dashboard/repository/UserRepo.java
+++ src/main/java/com/lead/dashboard/repository/UserRepo.java
@@ -15,4 +15,11 @@ public interface UserRepo extends JpaRepository<User,Long>
 
 	@Query(value = "SELECT * FROM user u WHERE u.email =:email", nativeQuery = true)
 	User findByemail(String email);
+	
+	@Query(value = "SELECT u.role FROM user u WHERE u.id =:id", nativeQuery = true)
+	List<String> findRoleById(Long id);
+	
+	@Query(value = " SELECT r.name FROM  user_role ur left join roles r on r.id=ur.user_role_id WHERE ur.user_id=:id", nativeQuery = true)
+	List<String> findRoleNameById(Long id);
+	
 }
