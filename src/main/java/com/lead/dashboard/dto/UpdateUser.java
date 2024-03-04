2a00fdd87efe19a71dd4f9b82d47631507478710
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-03-04 15:05:31 +0530

    update changesa

--- src/main/java/com/lead/dashboard/dto/UpdateUser.java
+++ src/main/java/com/lead/dashboard/dto/UpdateUser.java
@@ -8,10 +8,7 @@ public class UpdateUser {
 	
 	private Long id;
 	
-    private String firstName;
 
-    @NonNull
-    private String lastName;
     
     private String fullName;
 
@@ -35,23 +32,6 @@ public class UpdateUser {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
 	public String getFullName() {
 		return fullName;
 	}
