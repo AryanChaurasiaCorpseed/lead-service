a7919ca2ceba5bfd5dd65dfe5b706aef55521295
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-03-19 18:40:39 +0530

    implements changes

--- src/main/java/com/lead/dashboard/controller/projectController/PaymentDataController.java
+++ src/main/java/com/lead/dashboard/controller/projectController/PaymentDataController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lead.dashboard.domain.PaymentData;
 import com.lead.dashboard.domain.Project;
@@ -14,7 +15,7 @@ import com.lead.dashboard.dto.PaymentDataDto;
 import com.lead.dashboard.dto.UpdateDataDto;
 import com.lead.dashboard.service.PaymentDataService;
 import com.lead.dashboard.util.UrlsMapping;
-
+@RestController
 public class PaymentDataController {
 	
 	@Autowired
