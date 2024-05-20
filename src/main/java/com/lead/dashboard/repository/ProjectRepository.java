a2deb46d368618e5a9d2d4b1bf36a3cde73e72e5
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-05-20 16:01:27 +0530

    implements changes'

--- src/main/java/com/lead/dashboard/repository/ProjectRepository.java
+++ src/main/java/com/lead/dashboard/repository/ProjectRepository.java
@@ -1,11 +1,17 @@
 package com.lead.dashboard.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lead.dashboard.domain.Project;
 
 @Repository
 public interface ProjectRepository  extends JpaRepository<Project, Long> {
+
+	@Query(value = "SELECT * FROM project p WHERE p.id in(:projectId)", nativeQuery = true)
+	List<Project> findAllByIdIn(List<Long> projectId);
 
 }
--- src/main/java/com/lead/dashboard/serviceImpl/CompanyServiceImpl.java
+++ src/main/java/com/lead/dashboard/serviceImpl/CompanyServiceImpl.java
@@ -16,6 +16,7 @@ import com.lead.dashboard.domain.lead.Lead;
 import com.lead.dashboard.dto.CompanyDto;
 import com.lead.dashboard.repository.CompanyRepository;
 import com.lead.dashboard.repository.LeadRepository;
+import com.lead.dashboard.repository.ProjectRepository;
 import com.lead.dashboard.service.CompanyService;
 
 @Service
@@ -23,6 +24,10 @@ public class CompanyServiceImpl implements CompanyService {
 
 	@Autowired
 	CompanyRepository companyRepository;
+	
+	@Autowired
+	ProjectRepository projectRepository;
+	
 	@Autowired
 	LeadRepository leadRepository;
 	
@@ -39,6 +44,8 @@ public class CompanyServiceImpl implements CompanyService {
 		company.setCity(companyDto.getCity());
 		company.setCountry(companyDto.getCountry());
 		company.setState(companyDto.getState());
+		List<Project> projectList = projectRepository.findAllByIdIn(companyDto.getProjectId());
+		company.setCompanyProject(projectList);
 		List<Lead> leadList=leadRepository.findAllByIdIn(companyDto.getLeadId());
 		company.setCompanyLead(leadList);
 		companyRepository.save(company);
@@ -107,6 +114,47 @@ public class CompanyServiceImpl implements CompanyService {
         	flag = true;
          }
 		return flag;
+	}
+
+	@Override
+	public List<Map<String, Object>> getAllProjectByCompany(Long companyId) {
+		Optional<Company> companyOp = companyRepository.findById(companyId);
+		List<Map<String, Object>> result = new ArrayList<>();
+         if(companyOp.get()!=null) {
+        	 Company company = companyOp.get();
+        	 List<Project>projectList= company.getCompanyProject();
+        	 for(Project p:projectList) {
+        		 Map<String, Object> map = new HashMap<>();
+        		 map.put("projectId", p.getId());
+        		 map.put("projectName", p.getName());
+        		 map.put("assignee", p.getAssignee());
+        		 map.put("product", p.getProduct());
+        		 result.add(map);
+        	 }
+         }
+		return result;
+	}
+
+	@Override
+	public List<Map<String, Object>> getAllLeadByCompany(Long companyId) {
+		
+		Optional<Company> companyOp = companyRepository.findById(companyId);
+		List<Map<String, Object>> result = new ArrayList<>();
+         if(companyOp.get()!=null) {
+        	 Company company = companyOp.get();
+        	 List<Lead>leadList= company.getCompanyLead();
+        	 for(Lead l:leadList) {
+        		 Map<String, Object> map = new HashMap<>();
+        		 map.put("leadId", l.getId());
+        		 map.put("leadName", l.getName());
+        		 map.put("assignee", l.getAssignee());
+        		 map.put("client", l.getName());
+        		 map.put("ipAddress", l.getIpAddress());
+
+        		 result.add(map);
+        	 }
+         }
+		return result;
 	}
 
 }
