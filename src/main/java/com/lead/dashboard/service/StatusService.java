b931dabfc614f3d3cb0b7674e3db883a50185300
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-06-26 11:11:51 +0530

    changes in git

--- src/main/java/com/lead/dashboard/service/StatusService.java
+++ src/main/java/com/lead/dashboard/service/StatusService.java
@@ -2,6 +2,7 @@ package com.lead.dashboard.service;
 
 import com.lead.dashboard.domain.lead.LeadStatusChangeHistory;
 import com.lead.dashboard.dto.CreateLeadStatus;
+import com.lead.dashboard.dto.UpdateLeadStatusDto;
 import com.lead.dashboard.domain.Status;
 
 import java.util.List;
@@ -10,11 +11,11 @@ public interface StatusService {
     Status createStatus(CreateLeadStatus status);
     List<Status> getAllStatuses();
     Status getStatusById(Long id);
-    Status updateStatus(Status status);
     void deleteStatus(Long id);
 
     void updateLeadStatus(Long leadId, Long statusId,Long userId);
 
     List<LeadStatusChangeHistory> getStatusHistoryForLead(Long leadId);
 	List<Status> getAllPreviusStatus(Long statusId);
+	Status updateStatus(UpdateLeadStatusDto updateLeadStatusDto);
 }
