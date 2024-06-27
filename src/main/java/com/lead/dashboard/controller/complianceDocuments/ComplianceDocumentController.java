e1860f5a94d86899500a3dbe20b70a7ba73af69c
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-06-27 16:44:35 +0530

    implements changes

--- src/main/java/com/lead/dashboard/controller/complianceDocuments/ComplianceDocumentController.java
+++ src/main/java/com/lead/dashboard/controller/complianceDocuments/ComplianceDocumentController.java
@@ -14,6 +14,7 @@ import com.lead.dashboard.domain.ServiceDetails;
 import com.lead.dashboard.domain.product.Category;
 import com.lead.dashboard.repository.ServiceDetailsRepository;
 import com.lead.dashboard.repository.product.CategoryRepo;
+import com.lead.dashboard.service.ComplianceDocService;
 import com.lead.dashboard.util.UrlsMapping;
 
 @RestController
@@ -23,23 +24,12 @@ public class ComplianceDocumentController {
 	@Autowired
 	CategoryRepo categoryRepo; 
 	
+	@Autowired
+	ComplianceDocService complianceDocService;
+	
 	@GetMapping(UrlsMapping.GET_ALL_COMPLIANCE_DOCUMENTS)
 	public List<Map<String,Object>>getAllComplianceDocuments(){
-		List<Category> categoryList = categoryRepo.findAll();
-		List<Map<String,Object>> res = new ArrayList<>();
-		
-		for(Category category:categoryList) {
-			Map<String,Object>map = new HashMap<>();
-			map.put("id", category.getId());
-			map.put("name", category.getCategoryName());
-			List<String>doc = new ArrayList<>();
-			if(category.getDocuments()!=null) {
-				doc.addAll (category.getDocuments());
-			}
-			
-			map.put("documents", doc);
-			res.add(map);
-		}
+		List<Map<String,Object>>res=complianceDocService.getAllComplianceDocuments();	
 		return res;
 	}
 } 
