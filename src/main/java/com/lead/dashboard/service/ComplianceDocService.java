f1c25e4203ef7a7074cde144b0df24dce186bd94
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-06-27 18:11:46 +0530

    implements changes

--- src/main/java/com/lead/dashboard/service/ComplianceDocService.java
+++ src/main/java/com/lead/dashboard/service/ComplianceDocService.java
@@ -3,6 +3,9 @@ package com.lead.dashboard.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ComplianceDocService {
 
 	public List<Map<String,Object>>getAllComplianceDocuments();
