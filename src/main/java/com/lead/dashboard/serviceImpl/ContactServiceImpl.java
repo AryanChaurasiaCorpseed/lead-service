12cb2cc9bc581c8789559c024cecabe5f3b40b90
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-07-29 11:59:03 +0530

    implements changes

--- src/main/java/com/lead/dashboard/serviceImpl/ContactServiceImpl.java
+++ src/main/java/com/lead/dashboard/serviceImpl/ContactServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.lead.dashboard.domain.Contact;
+import com.lead.dashboard.dto.CreateContactDto;
 import com.lead.dashboard.repository.ContactRepo;
 import com.lead.dashboard.service.ContactService;
 
@@ -20,6 +21,21 @@ public class ContactServiceImpl implements ContactService{
 		// TODO Auto-generated method stub
 		List<Contact> contactList = contactRepo.findAll();
 		return contactList;
+	}
+	@Override
+	public Contact createContact(CreateContactDto createContactDto) {
+		Contact contact = new Contact();
+		contact.setName(createContactDto.getName());
+		contact.setEmails(createContactDto.getEmails());
+		contact.setWhatsappNo(createContactDto.getWhatsappNo());
+		contact.setContactNo(createContactDto.getContactNo());
+		contactRepo.save(contact);
+		return contact;
+	}
+	@Override
+	public Contact getContactById(Long id) {
+		Contact contact = contactRepo.findById(id).get();
+		return contact;
 	}
 
 }
