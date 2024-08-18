2590df3cf6110fb91dae2d17453c3b8f9909589f
Aryan Chaurasia <aryanchaurasia@Aryans-MacBook-Air.local>
2024-08-18 20:50:35 +0530

    add some changes

---
+++ src/main/java/com/lead/dashboard/domain/SubSubIndustry.java
@@ -0,0 +1,43 @@
+package com.lead.dashboard.domain;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.lead.dashboard.domain.product.Product;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table
+@Entity
+@Getter
+@Setter
+@Data
+public class SubSubIndustry {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)	
+    Long id ;
+	String name;
+	
+		@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	@JsonIgnore
+	@JoinTable(name="sub_sub_industry_industry_data",joinColumns = {@JoinColumn(name="sub_sub_industry_id",referencedColumnName="id",nullable=true)},
+			inverseJoinColumns = {@JoinColumn(name="sub_sub_industry_industry_data_id"
+					+ "",referencedColumnName = "id",nullable=true,unique=false)})
+	List<IndustryData>IndustryDataList;
+
+
+}
---
+++ src/main/java/com/lead/dashboard/repository/SubSubIndustryRepo.java
@@ -0,0 +1,9 @@
+package com.lead.dashboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lead.dashboard.domain.SubSubIndustry;
+
+public interface SubSubIndustryRepo extends JpaRepository<SubSubIndustry, Long> {
+
+}
