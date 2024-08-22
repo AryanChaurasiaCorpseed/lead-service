6196a522cfbafe2c985eb86572d0bd61f6aaba62
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-08-22 14:29:46 +0530

    add some changes

---
+++ src/main/java/com/lead/dashboard/controller/industryController/SubSubIndustryController.java
@@ -0,0 +1,39 @@
+package com.lead.dashboard.controller.industryController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lead.dashboard.domain.IndustryData;
+import com.lead.dashboard.domain.SubSubIndustry;
+import com.lead.dashboard.dto.SubSubIndustryDto;
+import com.lead.dashboard.service.SubSubIndustryService;
+@RestController
+@RequestMapping("/leadService/")
+public class SubSubIndustryController {
+
+	@Autowired
+	SubSubIndustryService subSubIndustryService;
+	
+	
+	
+	@PostMapping("/api/v1/industryData/createSubSubIndustry")
+	public Boolean createSubSubIndustry(@RequestBody SubSubIndustryDto subSubIndustryDto){	
+		Boolean res= subSubIndustryService.createSubSubIndustry(subSubIndustryDto);
+		return res;
+	}
+	
+	@GetMapping("/api/v1/industryData/getAllSubSubIndustry")
+	public List<SubSubIndustry> getAllIndustryData(){	
+		List<SubSubIndustry> res= subSubIndustryService.getAllSubSubIndustry();
+		return res;
+	}
+	
+	
+}
--- src/main/java/com/lead/dashboard/domain/SubIndustry.java
+++ src/main/java/com/lead/dashboard/domain/SubIndustry.java
@@ -1,9 +1,18 @@
 package com.lead.dashboard.domain;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
@@ -11,13 +20,45 @@ import lombok.Setter;
 
 @Table
 @Entity
-@Getter
-@Setter
-@Data
+@Getter 
 public class SubIndustry {
-	
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)	
-    Long id ;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	Long id ;
 	String name;
+
+	@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	@JsonIgnore
+	@JoinTable(name="sub_industry_sub_sub_industry",joinColumns = {@JoinColumn(name="sub_industry_id",referencedColumnName="id",nullable=true)},
+	inverseJoinColumns = {@JoinColumn(name="sub_industry_sub_sub_industry_id"
+			+ "",referencedColumnName = "id",nullable=true,unique=false)})
+	List<SubSubIndustry>SubSubIndustry;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<SubSubIndustry> getSubSubIndustry() {
+		return SubSubIndustry;
+	}
+
+	public void setSubSubIndustry(List<SubSubIndustry> subSubIndustry) {
+		SubSubIndustry = subSubIndustry;
+	}
+	
+	
+	
 }
