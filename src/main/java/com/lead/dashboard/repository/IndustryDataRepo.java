12eebd6c7bb8cfcd30f113e3e554699ab4c6ef1e
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-08-22 14:29:46 +0530

    add some changes

--- src/main/java/com/lead/dashboard/repository/IndustryDataRepo.java
+++ src/main/java/com/lead/dashboard/repository/IndustryDataRepo.java
@@ -1,9 +1,15 @@
 package com.lead.dashboard.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.lead.dashboard.domain.IndustryData;
 
 public interface IndustryDataRepo extends JpaRepository<IndustryData, Long> {
+
+	@Query(value = "SELECT * FROM industry_data i WHERE i.id in (:industryDataId)", nativeQuery = true)
+	List<IndustryData> findAllByIdIn(List<Long> industryDataId);
 
 }
