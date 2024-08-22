5d1cd2ef06c5e3b6f0d623a94be566e93da0c854
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-08-22 18:27:12 +0530

    implements changes

--- src/main/java/com/lead/dashboard/domain/Industry.java
+++ src/main/java/com/lead/dashboard/domain/Industry.java
@@ -1,9 +1,18 @@
 package com.lead.dashboard.domain;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
@@ -20,4 +29,37 @@ public class Industry {
     @GeneratedValue(strategy = GenerationType.IDENTITY)	
     Long id ;
 	String name;
+	
+	@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	@JsonIgnore
+	@JoinTable(name="industry_sub_industry",joinColumns = {@JoinColumn(name="industry_id",referencedColumnName="id",nullable=true)},
+	inverseJoinColumns = {@JoinColumn(name="industry_sub_industry_id"
+			+ "",referencedColumnName = "id",nullable=true,unique=false)})
+	List<SubIndustry>subIndustry;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<SubIndustry> getSubIndustry() {
+		return subIndustry;
+	}
+
+	public void setSubIndustry(List<SubIndustry> subIndustry) {
+		this.subIndustry = subIndustry;
+	}
+	
+	
 }
---
+++ src/main/java/com/lead/dashboard/serviceImpl/IndustryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.lead.dashboard.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.lead.dashboard.service.IndustryService;
+@Service
+public class IndustryServiceImpl implements IndustryService{
+
+}
