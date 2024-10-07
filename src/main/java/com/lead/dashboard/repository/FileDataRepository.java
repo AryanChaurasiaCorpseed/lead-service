9335a990b8a507a8af36f58781f00ff85a8fdd1d
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-10-07 11:43:30 +0530

    add some changes

--- src/main/java/com/lead/dashboard/repository/FileDataRepository.java
+++ src/main/java/com/lead/dashboard/repository/FileDataRepository.java
@@ -15,5 +15,8 @@ public interface FileDataRepository extends JpaRepository<FileData, Long> {
 	
 	@Query(value = "SELECT * FROM file_data fd WHERE fd.file_path in(:fileName)", nativeQuery = true)
 	List<FileData> findByfilePathIn(List<String> fileName);
+	
+	@Query(value = "SELECT * FROM file_data fd WHERE fd.name =:fileName limit 1", nativeQuery = true)
+	FileData findByNameExist(String fileName);
 
 }
--- src/main/java/com/lead/dashboard/serviceImpl/FileUploadServiceImpl.java
+++ src/main/java/com/lead/dashboard/serviceImpl/FileUploadServiceImpl.java
@@ -8,6 +8,7 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -19,6 +20,7 @@ import org.springframework.util.FileSystemUtils;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.fasterxml.jackson.databind.util.NativeImageUtil;
 import com.lead.dashboard.config.AwsBlobAdapter;
 import com.lead.dashboard.config.AzureBlobAdapter;
@@ -28,106 +30,113 @@ import com.lead.dashboard.service.FileUploadService;
 
 @Service
 public class FileUploadServiceImpl implements FileUploadService{
-	
-	@Autowired
-    private AzureBlobAdapter azureAdapter;
-	
+
+	//	@Autowired
+	//    private AzureBlobAdapter azureAdapter;
+	//	
 	@Autowired
 	AwsBlobAdapter awsBlobAdapter;
-	
+
 	@Autowired
 	FileDataRepository fileDataRepository;
-	
-//	public String UPLOAD_DIR="/Users/aryanchaurasia/Documents/Corpseed-img";
+
+	//	public String UPLOAD_DIR="/Users/aryanchaurasia/Documents/Corpseed-img";
 	public String UPLOAD_DIR="C:/Users/user/Documents/imageTest/image (1)";
-//    public final String FOLDER_PATH="C:/Users/user/Documents/images/";
-//    public final String PROD_PATH="https://demo003.blob.core.windows.net/test3/";
-//    public final String PROD_PATH="https://recordplus.blob.core.windows.net/eeptest/";
-//    public final String PROD_PATH="https://corpseeds.blob.core.windows.net/corpseed-erp/";
-    public final String PROD_PATH= "https://corpseed-test.s3.ap-south-1.amazonaws.com/";
+	//    public final String FOLDER_PATH="C:/Users/user/Documents/images/";
+	//    public final String PROD_PATH="https://demo003.blob.core.windows.net/test3/";
+	//    public final String PROD_PATH="https://recordplus.blob.core.windows.net/eeptest/";
+	//    public final String PROD_PATH="https://corpseeds.blob.core.windows.net/corpseed-erp/";
+	public final String PROD_PATH= "https://corpseed-test.s3.ap-south-1.amazonaws.com/";
 
-	 public boolean uploadFilesData( MultipartFile multipartFile) {
-		 
-		    boolean f=false;
-		    try {
-		    	InputStream ls=multipartFile.getInputStream();
-		    	byte data[]=new byte[ls.available()];
-		    	ls.read(data);
-		    	
-		    	FileOutputStream fos=new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
-		    	fos.write(data);
-		    	fos.flush();
-		    	fos.close();
-		    	f=true;
-		    	
-		    }catch(Exception e) {
-		    	
-		    }
+	public boolean uploadFilesData( MultipartFile multipartFile) {
 
-		    return f;
+		boolean f=false;
+		try {
+			InputStream ls=multipartFile.getInputStream();
+			byte data[]=new byte[ls.available()];
+			ls.read(data);
+
+			FileOutputStream fos=new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
+			fos.write(data);
+			fos.flush();
+			fos.close();
+			f=true;
+
+		}catch(Exception e) {
+
+		}
+
+		return f;
 
 
-	  }
+	}
 
 	@Override
 	public String[] getFilesData() { 
-		        String folderPath = System.getProperty("user.dir") +"/Uploads"; 
-		        File directory= new File(UPLOAD_DIR); 
-		        String[] filenames = directory.list(); 
-		        return filenames;		
+		String folderPath = System.getProperty("user.dir") +"/Uploads"; 
+		File directory= new File(UPLOAD_DIR); 
+		String[] filenames = directory.list(); 
+		return filenames;		
 	}
 
-	
-	
 
-	
+
+
+
 	public String uploadImageToFileData(MultipartFile file) throws IllegalStateException, IOException {
-		
-//		String filePath=PROD_PATH+file.getOriginalFilename();
 
-//		String s=azureAdapter.uploadv2(file, 0);
-		String s =awsBlobAdapter.uploadAws(file, 0);
+		//		String filePath=PROD_PATH+file.getOriginalFilename();
+
+		//		String s=azureAdapter.uploadv2(file, 0);
+		long d = new Date().getTime();
+		String s =awsBlobAdapter.uploadAws(file, d);
 		String filePath=PROD_PATH+s;
 		FileData fileData = new FileData();
 		fileData.setName(s);
 		fileData.setType(file.getContentType());
 		fileData.setFilePath(filePath);
-		fileDataRepository.save(fileData);
-		System.out.println("UPLOAD IMAGE .."+file.getContentType());
-//		file.transferTo(new File(filePath));
+		String fs = isFileExist(s);
+		if(isFileExist(s)==null) {
+			fileDataRepository.save(fileData);
+		}else {
+			filePath=PROD_PATH+fs;
+
+		}
+
+		//		file.transferTo(new File(filePath));
 		if(filePath!=null) {
 			return filePath;
 		}
 		return null;
-		
+
 	}
 
-	
+
 	public String uploadImageToFileDataV2(MultipartFile file) throws IllegalStateException, IOException {
 		String filePath=PROD_PATH+file.getOriginalFilename();
-		
-//		String filePath=PROD_PATH+file.getOriginalFilename();
 
-		FileData fileData = new FileData();
-		fileData.setName(file.getOriginalFilename());
-		fileData.setType(file.getContentType());
-		fileData.setFilePath(filePath);
-		fileDataRepository.save(fileData);
-		String s=azureAdapter.upload(file, 0);
-		System.out.println("UPLOAD IMAGE .."+s);
-//		file.transferTo(new File(filePath));
-		if(filePath!=null) {
-			return filePath;
-		}
+		//		String filePath=PROD_PATH+file.getOriginalFilename();
+
+		//		FileData fileData = new FileData();
+		//		fileData.setName(file.getOriginalFilename());
+		//		fileData.setType(file.getContentType());
+		//		fileData.setFilePath(filePath);
+		//		fileDataRepository.save(fileData);
+		//		String s=azureAdapter.upload(file, 0);
+		//		System.out.println("UPLOAD IMAGE .."+s);
+		////		file.transferTo(new File(filePath));
+		//		if(filePath!=null) {
+		//			return filePath;
+		//		}
 		return null;
-		
+
 	}
-	
+
 	public byte[] downloadImageToFileSystem(String fileName) throws IOException {
 		Optional<FileData> fileData = fileDataRepository.findByName(fileName);
 		String filePath = fileData.get().getFilePath();
-	    byte[] images = Files.readAllBytes(new File(filePath).toPath());
-	    return images;
+		byte[] images = Files.readAllBytes(new File(filePath).toPath());
+		return images;
 	}
 
 	@Override
@@ -139,6 +148,16 @@ public class FileUploadServiceImpl implements FileUploadService{
 		}
 		return image;
 	}
-	
-	
+
+	private String isFileExist(String fileName) {
+
+		FileData fileData = fileDataRepository.findByNameExist(fileName);
+		String image=null;
+		if(fileData!=null) {
+			image=fileData.getName();
+		}
+		return image;
+	}
+
+
 }
